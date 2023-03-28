6eaaca3b70c42177884ac769e3dbd234b703d988
Ridoyb <ridoyb8730@gmail.com>
2023-03-29 05:40:19 +0600

    Header added

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.scss'
+import video from '../../Assets/video.mp4'
+
+const Home = () => {
+    return (
+        <section className='home'>
+            <div className="overly"></div>
+            <video src={video} muted autoPlay loop type="video/mp4"></video>
+        </section>
+    );
+};
+
+export default Home;
