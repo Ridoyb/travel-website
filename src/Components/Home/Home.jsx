3bdcd2e0046abc8a763d821d6731943e3ed8214a
Ridoyb <ridoyb8730@gmail.com>
2023-03-29 06:15:51 +0600

    Basic booking from added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './home.scss'
 import video from '../../Assets/video.mp4'
+import { GrLocation } from 'react-icons/gr';
 
 const Home = () => {
     return (
@@ -9,7 +10,43 @@ const Home = () => {
             <video src={video} muted autoPlay loop type="video/mp4"></video>
 
             <div className="homeContent container">
+                <div className="textDiv">
+                    <span className='smallText'>
+                        Our Packages
+                    </span>
+                    <h1 className='homeTitle'>
+                        Search Your Holidays
+                    </h1>
+                </div>
 
+             <div className="cardDiv grid">
+                <div className="destinationInput">
+                    <label htmlFor="city">Search Your Destination:</label>
+                    <div className="input flex">
+                        <input type="text" placeholder='Enter Name here..' />
+                        <GrLocation className='icon'/>
+                    </div>
+                </div>
+            </div>   
+             <div className="cardDiv grid">
+                <div className="dateInput">
+                    <label htmlFor="city">Select Your Date:</label>
+                    <div className="input flex">
+                        <input type="date" />
+                    </div>
+                </div>
+            </div>   
+             <div className="cardDiv grid">
+                <div className="priceInput">
+                    <div className="label_total flex">
+                        <label htmlFor="price">Max Price:</label>
+                        <h3 className='total'>$5000</h3>
+                    </div>
+                    <div className="input flex">
+                        <input type="range" max="5000" min="1000"/>
+                    </div>
+                </div>
+            </div>   
             </div>
         </section>
     );
